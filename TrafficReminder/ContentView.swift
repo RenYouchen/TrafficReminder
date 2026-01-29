6724df141568926e074b5f3918888088c7dca150
renyouchen <renyouchen3@gmail.com>
2026-01-29 20:21:10 +0800

    Initial Commit

---
+++ TrafficReminder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrafficReminder
+//
+//  Created by BrianRen on 2026/1/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrafficReminder/TrafficReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficReminderApp.swift
+//  TrafficReminder
+//
+//  Created by BrianRen on 2026/1/29.
+//
+
+import SwiftUI
+
+@main
+struct TrafficReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
